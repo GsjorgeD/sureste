93d979a2bddc039000275f568b7c0c25a41f5176
Jorge Daniel Gutierrez <jdaniel.vivendo@gmail.com>
2021-05-23 22:48:31 -0500

    Finish Sureste app

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :new_vacantes
   resources :vacantes
-  get 'static_pages/index'
-  get 'static_pages/info'
+  get '/index', to: 'static_pages#index'
+  get '/info', to:'static_pages#info'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root 'static_pages#index'
--- db/seeds.rb
+++ db/seeds.rb
@@ -24,38 +24,150 @@ vacantes = Vacante.create([
   categoria: "Programacion"
 },
 {
-  posicion: "Desarrollador Web",
-  descripcion: "Se solicita",
+  posicion: "Becario",
+  descripcion: "Ingeniero en Sistemas/ Computación o Área a fin.Continuar estudiando, preferentemente en horarios vespertino/nocturno o sabatino.Cursar 8vo. semestre de tu carrera (1 año para terminar créditos).Amplio interés en el Desarrollo",
+  ubicacion: "Remoto",
+  salario: 2000,
+  tipo: "Medio tiempo",
+  categoria: "Programacion"
+},
+{
+  posicion: "Practicante desarrollador web",
+  descripcion: "Ingeniero en Sistemas/ Computación o Área a fin.Continuar estudiando, preferentemente en horarios vespertino/nocturno o sabatino.Cursar 8vo. semestre de tu carrera (1 año para terminar créditos).Amplio interés en el Desarrollo",
+  ubicacion: "Remoto",
+  salario: 2000,
+  tipo: "Medio tiempo",
+  categoria: "Desarrollo web"
+},
+{
+  posicion: "Front end",
+  descripcion: "Se solicita programador con conocimienetos en Html, CSS y JavaScript. Opcional conocimientos en Frameworks.",
   ubicacion: "Remoto",
   salario: 4000,
-  tipo: "tiempo completo",
+  tipo: "Tiempo completo",
+  categoria: "Desarrollo web"
+},
+{
+  posicion: "Back end",
+  descripcion: "Se solicita programador con conocimentos en uno varios lenguages como Go,Ruby y Java. Opcional conocimientos en creacion de APIS.",
+  ubicacion: "Remoto",
+  salario: 6000,
+  tipo: "Tiempo completo",
+  categoria: "Desarrollo web"
+},
+{
+  posicion: "Becario",
+  descripcion: "Ingeniero en Sistemas/ Computación o Área a fin.Continuar estudiando, preferentemente en horarios vespertino/nocturno o sabatino.Cursar 8vo. semestre de tu carrera (1 año para terminar créditos).Amplio interés en el Desarrollo",
+  ubicacion: "Cd del Carmen",
+  salario: 2000,
+  tipo: "Medio tiempo",
+  categoria: "Programacion"
+},
+{
+  posicion: "Practicante",
+  descripcion: "Ingeniero en Sistemas/ Computación o Área a fin.Continuar estudiando, preferentemente en horarios vespertino/nocturno o sabatino.Cursar 8vo. semestre de tu carrera (1 año para terminar créditos).Amplio interés en el Desarrollo",
+  ubicacion: "Cd del carmen",
+  salario: 2000,
+  tipo: "Medio tiempo",
+  categoria: "Desarrollo web"
+},
+{
+  posicion: "Ingeniero en sistemas",
+  descripcion: "Ingeniero en Sistemas/ Computación o Área a fin.Continuar estudiando, preferentemente en horarios vespertino/nocturno o sabatino.Cursar 8vo. semestre de tu carrera (1 año para terminar créditos).Amplio interés en el Desarrollo",
+  ubicacion: "Cd del carmen",
+  salario: 2000,
+  tipo: "Medio tiempo",
+  categoria: "Ingenieria"
+},
+{
+  posicion: "Practicante",
+  descripcion: "Ingeniero en Sistemas/ Computación o Área a fin.Continuar estudiando, preferentemente en horarios vespertino/nocturno o sabatino.Cursar 8vo. semestre de tu carrera (1 año para terminar créditos).Amplio interés en el Desarrollo",
+  ubicacion: "Remoto",
+  salario: 4000,
+  tipo: "Medio tiempo",
   categoria: "Programacion"
 }
   ])
 
   new_vacantes = NewVacante.create([
-  {
-    posicion: "Programdor Front end",
-    descripcion: "Se solicita programador con conocimienetos en Html, CSS y JavaScript. Opcional conocimientos en Frameworks.",
-    ubicacion: "Remoto",
-    salario: 4000,
-    tipo: "Tiempo completo",
-    categoria: "Programacion"
-  },
-  {
-    posicion: "Programdor Back end",
-    descripcion: "Se solicita programador con conocimentos en uno varios lenguages como Go,Ruby y Java. Opcional conocimientos en creacion de APIS.",
-    ubicacion: "Remoto",
-    salario: 6000,
-    tipo: "Tiempo completo",
-    categoria: "Programacion"
-  },
-  {
-    posicion: "Desarrollador Web",
-    descripcion: "Se solicita",
-    ubicacion: "Remoto",
-    salario: 4000,
-    tipo: "tiempo completo",
-    categoria: "Programacion"
-  }
-    ])
+    {
+      posicion: "Programdor Front end",
+      descripcion: "Se solicita programador con conocimienetos en Html, CSS y JavaScript. Opcional conocimientos en Frameworks.",
+      ubicacion: "Remoto",
+      salario: 4000,
+      tipo: "Tiempo completo",
+      categoria: "Programacion"
+    },
+    {
+      posicion: "Programdor Back end",
+      descripcion: "Se solicita programador con conocimentos en uno varios lenguages como Go,Ruby y Java. Opcional conocimientos en creacion de APIS.",
+      ubicacion: "Remoto",
+      salario: 6000,
+      tipo: "Tiempo completo",
+      categoria: "Programacion"
+    },
+    {
+      posicion: "Becario",
+      descripcion: "Ingeniero en Sistemas/ Computación o Área a fin.Continuar estudiando, preferentemente en horarios vespertino/nocturno o sabatino.Cursar 8vo. semestre de tu carrera (1 año para terminar créditos).Amplio interés en el Desarrollo",
+      ubicacion: "Remoto",
+      salario: 2000,
+      tipo: "Medio tiempo",
+      categoria: "Programacion"
+    },
+    {
+      posicion: "Practicante desarrollador web",
+      descripcion: "Ingeniero en Sistemas/ Computación o Área a fin.Continuar estudiando, preferentemente en horarios vespertino/nocturno o sabatino.Cursar 8vo. semestre de tu carrera (1 año para terminar créditos).Amplio interés en el Desarrollo",
+      ubicacion: "Remoto",
+      salario: 2000,
+      tipo: "Medio tiempo",
+      categoria: "Desarrollo web"
+    },
+    {
+      posicion: "Front end",
+      descripcion: "Se solicita programador con conocimienetos en Html, CSS y JavaScript. Opcional conocimientos en Frameworks.",
+      ubicacion: "Remoto",
+      salario: 4000,
+      tipo: "Tiempo completo",
+      categoria: "Desarrollo web"
+    },
+    {
+      posicion: "Back end",
+      descripcion: "Se solicita programador con conocimentos en uno varios lenguages como Go,Ruby y Java. Opcional conocimientos en creacion de APIS.",
+      ubicacion: "Remoto",
+      salario: 6000,
+      tipo: "Tiempo completo",
+      categoria: "Desarrollo web"
+    },
+    {
+      posicion: "Becario",
+      descripcion: "Ingeniero en Sistemas/ Computación o Área a fin.Continuar estudiando, preferentemente en horarios vespertino/nocturno o sabatino.Cursar 8vo. semestre de tu carrera (1 año para terminar créditos).Amplio interés en el Desarrollo",
+      ubicacion: "Cd del Carmen",
+      salario: 2000,
+      tipo: "Medio tiempo",
+      categoria: "Programacion"
+    },
+    {
+      posicion: "Practicante",
+      descripcion: "Ingeniero en Sistemas/ Computación o Área a fin.Continuar estudiando, preferentemente en horarios vespertino/nocturno o sabatino.Cursar 8vo. semestre de tu carrera (1 año para terminar créditos).Amplio interés en el Desarrollo",
+      ubicacion: "Cd del carmen",
+      salario: 2000,
+      tipo: "Medio tiempo",
+      categoria: "Desarrollo web"
+    },
+    {
+      posicion: "Ingeniero en sistemas",
+      descripcion: "Ingeniero en Sistemas/ Computación o Área a fin.Continuar estudiando, preferentemente en horarios vespertino/nocturno o sabatino.Cursar 8vo. semestre de tu carrera (1 año para terminar créditos).Amplio interés en el Desarrollo",
+      ubicacion: "Cd del carmen",
+      salario: 2000,
+      tipo: "Medio tiempo",
+      categoria: "Ingenieria"
+    },
+    {
+      posicion: "Practicante",
+      descripcion: "Ingeniero en Sistemas/ Computación o Área a fin.Continuar estudiando, preferentemente en horarios vespertino/nocturno o sabatino.Cursar 8vo. semestre de tu carrera (1 año para terminar créditos).Amplio interés en el Desarrollo",
+      ubicacion: "Remoto",
+      salario: 4000,
+      tipo: "Medio tiempo",
+      categoria: "Programacion"
+    }
+      ])
